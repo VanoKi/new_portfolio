150d07b271b673bc3ad22793092de2ad34332636
VanoKi <kirsvano@gmail.com>
2024-06-05 16:36:07 +0300

    feat: create a json file

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function screen($url, $extn, $size, $format) 
-{
-    $url = 'http://mini.s-shot.ru/'.$extn.'/'.$size.'/'.$format.'/?'.urlencode($url);
-    $str = file_get_contents($url);
-    file_put_contents('screen.'.$format, $str); // тут лучше указать путь куда сохранять
-}
+// function screen($url, $extn, $size, $format) 
+// {
+//     $url = 'http://mini.s-shot.ru/'.$extn.'/'.$size.'/'.$format.'/?'.urlencode($url);
+//     $str = file_get_contents($url);
+//     file_put_contents('screen.'.$format, $str); // тут лучше указать путь куда сохранять
+// }
 
-screen('http://hashcode.ru', '1024x768', '600', 'jpeg');
+// screen('http://hashcode.ru', '1024x768', '600', 'jpeg');
+
