6b601116bc08c988abf7fb3089b8ef71ef4c3757
VanoKi <kirsvano@gmail.com>
2024-06-07 23:41:43 +0300

    feat: add more items

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 import {data} from './data.js'
-let start = 3
-let part = data.slice(0, start)
-let block = ``
-for (let index = 0; index < part.length; index++) {
-  block += `<div class="item">
-    <img class="portfolio_img" src="${part[index].image}" alt="Caffee_preview">
-    <h3><a href="${part[index].url}">${part[index]['site-name']}</a></h3>
-  </div>`
+let currInd = 0
+let itemsPerPage = 3
+const moreBtn = document.querySelector('.btn_more')
+
+function loadItems() {
+  const part = data.slice(currInd, currInd + itemsPerPage)
+  let block = ``
+  for (let index = 0; index < part.length; index++) {
+    block += `<div class="item">
+      <img class="portfolio_img" src="${part[index].image}" alt="Caffee_preview">
+      <h3><a href="${part[index].url}">${part[index]['site-name']}</a></h3>
+    </div>`
+  }
+  document.querySelector('.container').innerHTML += block
+  currInd += itemsPerPage
+
+  if (currInd >= data.length){
+    moreBtn.style.display = 'none'
+  }
 }
-
-document.querySelector('.container').innerHTML = block
-
+moreBtn.addEventListener('click', loadItems)
+loadItems()
