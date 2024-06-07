f4c6aa7669e3db052b474d419f610cb71f74c1fc
VanoKi <kirsvano@gmail.com>
2024-06-07 22:46:56 +0300

    feat: add btn more

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ let start = 3
 let part = data.slice(0, start)
 let block = ``
 for (let index = 0; index < part.length; index++) {
-  // document.querySelector('.container').innerHTML = `
-  //   <div class="item">
-  //     <img class="portfolio_img" src="${part[index].image}" alt="Caffee_preview">
-  //     <h3><a href="${part[index].url}">${part[index]['site-name']}</a></h3>
-  //   </div>`
   block += `<div class="item">
     <img class="portfolio_img" src="${part[index].image}" alt="Caffee_preview">
     <h3><a href="${part[index].url}">${part[index]['site-name']}</a></h3>
